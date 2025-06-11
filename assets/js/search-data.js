1b1a9dc78d1b6996ce01c856bfcd450c4a4185e2
shah4397 <sshah@central.uh.edu>
2025-06-11 20:34:50 +0000

    Deploying to gh-pages from @ shishirkshah/shishirkshah.github.io@98a2c15e3ab18c5c5d37ae0a4c242792e45da2ff 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -32,7 +32,7 @@ ninja.data = [{
           },
         },{id: "nav-teaching",
           title: "teaching",
-          description: "Materials for courses you taught. Replace this text with your description.",
+          description: "Courses taught.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/teaching/";
